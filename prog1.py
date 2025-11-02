18579ce453a5ea04186ed651552e524354555ece
SakethDodda12 <sakethdodda57@gmail.com>
2025-11-01 20:28:45 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,10 @@
+class Vehicule:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+class Bus(Vehicule):
+    pass
+School_bus = Bus("School Volvo", 180, 12)
+
+print("Vehiule name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
